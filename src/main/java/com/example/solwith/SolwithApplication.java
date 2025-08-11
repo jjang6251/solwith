7684381735ed6bf41fb6ed1a28343e38f6e12176
장성원 <jjang6251@gmail.com>
2025-08-11 18:21:26 +0900

    Initial commit

---
+++ src/main/java/com/example/solwith/SolwithApplication.java
@@ -0,0 +1,13 @@
+package com.example.solwith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolwithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolwithApplication.class, args);
+    }
+
+}
