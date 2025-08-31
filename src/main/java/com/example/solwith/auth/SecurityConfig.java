d5e45ded39c881335313ebbe6928ff8eb846aa91
장성원 <jjang6251@gmail.com>
2025-08-31 17:25:14 +0900

    @PreAuthorize, @PostAuthorize 사용하기 위한 SecurityConfig 수정 -> 메서드 보안 활성화

--- src/main/java/com/example/solwith/auth/SecurityConfig.java
+++ src/main/java/com/example/solwith/auth/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.solwith.auth;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,6 +13,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true) // @PreAuthorize, @PostAuthorize 사용
 public class SecurityConfig {
     private final JwtProvider jwtProvider;
     public SecurityConfig(JwtProvider jwtProvider) { this.jwtProvider = jwtProvider; }
